55ded712d63948b1f5cc431cbc36f6175d9f0336
Prabodha Srimal <prabodha007@gmail.com>
2015-02-14 01:33:12 +0530

    screip changes

--- modules/siddhi-samples/performance-samples/multiplequery.sh
+++ modules/siddhi-samples/performance-samples/multiplequery.sh
@@ -9,22 +9,24 @@ JVM=/usr/lib/jvm/java-6-sun-1.6.0.26/jre/bin/java
 
 CLASSPATH=${SIDDHI_ROOT}/modules/siddhi-samples/performance-samples/target/build/classes:${SIDDHI_ROOT}/modules/siddhi-samples/target/lib/antlr4-runtime-4.3.jar:${SIDDHI_ROOT}/modules/siddhi-samples/target/lib/commons-cli-1.2.jar:${SIDDHI_ROOT}/modules/siddhi-samples/target/lib/disruptor-3.2.1.jar:${SIDDHI_ROOT}/modules/siddhi-samples/target/lib/log4j-1.2.14.jar:${SIDDHI_ROOT}/modules/siddhi-samples/target/lib/siddhi-core-3.0.0-SNAPSHOT.jar:${SIDDHI_ROOT}/modules/siddhi-samples/target/lib/siddhi-query-api-3.0.0-SNAPSHOT.jar:${SIDDHI_ROOT}/modules/siddhi-samples/target/lib/siddhi-query-compiler-3.0.0-SNAPSHOT.jar:${SIDDHI_ROOT}/modules/siddhi-query-api/target/siddhi-query-api-3.0.0-SNAPSHOT.jar:${SIDDHI_ROOT}/modules/siddhi-query-compiler/target/siddhi-query-compiler-3.0.0-SNAPSHOT.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/antlr4-annotations.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/antlr4-runtime.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/hamcrest-core.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/javacpp-cudaclib-linux-x86_64.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/javacpp-cudaclib.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/javacpp.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/junit.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/log4j.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/mvel2.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/org.abego.treelayout.core.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/siddhi-query-api.jar:${SIDDHI_ROOT}/modules/siddhi-gpu/target/siddhi-query-compiler.jar:${SIDDHI_ROOT}/modules/siddhi-core/target/siddhi-core-3.0.0-SNAPSHOT.jar:${SIDDHI_ROOT}/modules/siddhi-samples/performance-samples/src/main/resources/log4j.properties
 
-APP=org.wso2.siddhi.performance.ComplexFilterMultipleQueryPerformance
-LOG=logs/ComplexFilterMultipleQueryPerformance/ComplexFilterMultipleQueryPerformance
+APP=org.wso2.siddhi.performance.JoinMultipleQueryPerformance
+LOG=logs/JoinMultipleQueryPerformance/JoinMultipleQueryPerformance
+#APP=org.wso2.siddhi.performance.ComplexFilterMultipleQueryPerformance
+#LOG=logs/ComplexFilterMultipleQueryPerformance/ComplexFilterMultipleQueryPerformance
 
 a="false"
 g="true"
-r=2048
+r=1024
 t=16
 b=128
 c=$((5000000 * r))
-q=2
+q=1
 
 #echo "${JVM} ${AGENTLIB} -classpath ${CLASSPATH} ${APP} --enable-async true --enable-gpu false --event-count ${c} --ringbuffer-size ${r} --threadpool-size ${t} --events-per-tblock ${b} &>${LOG}_gpu_${r}_${t}_${b}.log"
 #${JVM} ${AGENTLIB} -classpath ${CLASSPATH} ${APP} --enable-async false --enable-gpu false --event-count ${c} --ringbuffer-size ${r} --threadpool-size ${t} --events-per-tblock ${b} &>${LOG}_cpu_singlethread_${r}_${t}_${b}.log
-#${JVM} ${AGENTLIB} -classpath ${CLASSPATH} ${APP} --enable-async false --enable-gpu false --event-count ${c} --ringbuffer-size ${r} --threadpool-size ${t} --events-per-tblock ${b} &>${LOG}_cpu_singlethread_${r}_${t}_${b}.log
-#${JVM} ${AGENTLIB} -classpath ${CLASSPATH} ${APP} --enable-async true --enable-gpu false --event-count ${c} --ringbuffer-size ${r} --threadpool-size ${t} --events-per-tblock ${b} &>${LOG}_cpu_${r}_${t}_${b}.log
-${JVM} ${AGENTLIB} -classpath ${CLASSPATH} ${APP} --enable-async true --enable-gpu true --query-count ${q} --event-count ${c} --ringbuffer-size ${r} --threadpool-size ${t} --events-per-tblock ${b} #&>${LOG}_gpu_${r}_${t}_${b}.log
+#${JVM} ${AGENTLIB} -classpath ${CLASSPATH} ${APP} --enable-async false --enable-gpu false --event-count ${c} --ringbuffer-size ${r} --threadpool-size ${t} --events-per-tblock ${b} #&>${LOG}_cpu_singlethread_${r}_${t}_${b}.log
+#${JVM} ${AGENTLIB} -classpath ${CLASSPATH} ${APP} --enable-async true --enable-gpu false --event-count ${c} --ringbuffer-size ${r} --threadpool-size ${t} --events-per-tblock ${b} #&>${LOG}_cpu_${r}_${t}_${b}.log
+${JVM} ${AGENTLIB} -classpath ${CLASSPATH} ${APP} --enable-async true --enable-gpu true --query-count ${q} --event-count ${c} --ringbuffer-size ${r} --threadpool-size ${t} --events-per-tblock ${b} &>${LOG}_gpu_${r}_${t}_${b}.log
 
 exit
 
